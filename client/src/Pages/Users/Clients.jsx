7cada66cbc9058794402bc4477689c05e211bfbc
vurnkambal <verneilkent.batiller@gmail.com>
2026-02-25 00:11:18 +0800

    Add EditClient component for client details editing functionality

--- client/src/Pages/Users/Clients.jsx
+++ client/src/Pages/Users/Clients.jsx
@@ -13,6 +13,7 @@ import { Dropdown, Menu, MenuButton, MenuItem, menuItemClasses } from "@mui/base
 import Filter from "./Filter";
 import User from "./User";
 import DeleteClient from "./Delete";
+import EditClient from "./EditClient";
 
 const blue = {
   100: "#DAECFF",
@@ -139,6 +140,12 @@ const Clients = () => {
       headerClassName: "super-app-theme--header",
       renderCell: (params) => (
         <div className="flex gap-[10px]">
+          <Tooltip placement="top" title="Edit" arrow>
+            <CiEdit
+              onClick={() => handleOpenEditModal(params.row)}
+              className="cursor-pointer text-blue-500 text-[23px] hover:text-blue-400"
+            />
+          </Tooltip>
           {
             loggedUser?.role != 'employee' &&
             <Tooltip placement="top" title="Delete" arrow>
@@ -187,6 +194,7 @@ const Clients = () => {
     <div className="w-full">
 
       <DeleteClient open={openDeleteModal} setOpen={setOpenDeleteModal} userId={selectedUserId} />
+      <EditClient open={openEditModal} setOpen={setOpenEditModal} />
       <Filter open={openFilters} setOpen={setOpenFilters} />
       <User open={openUser} setOpen={setOpenUser} />
 
